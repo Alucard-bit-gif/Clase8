cb543c81d79c77ac6d0ffdd1c553f0c992981050
Sebastián <serviciossociologicos@gmail.com>
2020-11-15 17:32:18 -0500

    Se han declarado variables y constantes .js

--- GeeK_Lab_3/Geek_lab_3_pb-master/JS/core.js
+++ GeeK_Lab_3/Geek_lab_3_pb-master/JS/core.js
@@ -1,2 +1,17 @@
-
 alert("Programar el calculo del servicio")
+
+//Laboratorio Geek # 3//
+
+//Inicio
+
+//Declarar Variables
+
+let horasParqueo;
+let motoX;
+let carroY;
+const precioA = 5000;
+const precioB = 4000;
+const precioC = 3000;
+const descAuto = 0.10;
+const descMoto = 0.15;
+
