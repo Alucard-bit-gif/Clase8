543ba881ca18830fb9e7bab18d7eb31c741f05ea
Sebastián <serviciossociologicos@gmail.com>
2020-11-16 17:55:54 -0500

    Se continua verificando bloques de codigo

--- TestFolder/test.js
+++ TestFolder/test.js
@@ -7,15 +7,24 @@ let horasParqueo;
 let valorParqueo;
 let valorInicial;
 let porcentDesc = 0.10;
+let porcentDescB = 0.15;
 let valorDescuento;
 let totalaPagar;
 let precioA = 2000;
+let precioB = 5000;
+
 
 function Calcular()
 {
     //Leer Variables
+    
+
+    
     //Ingresar horas parqueo
     horasParqueo = Number(document.getElementById('time').value);
+    //Ingresar tipo Vehiculo
+    //tipoVehiculo = document.getElementById('tipo').value;
+    
 
     //Invocar funcion
     totalaPagar = pagoParqueo();
@@ -23,17 +32,16 @@ function Calcular()
     //valorInicial = precioPark();
 
     //Imprimir
+    document.getElementById('Inicial').value= horasParqueo*precioB;    
+    document.getElementById('Descuento').value= valorInicial*porcentDescB;    
     document.getElementById('Total').value= valorInicial - valorDescuento;    
-
+    console.log(totalaPagar);
 }
    //Procedimiento
 
-   function pagoParqueo()
-        {
-            valorInicial = horasParqueo*precioA;
-            valorDescuento = valorInicial*porcentDesc;
-            totalaPagar = valorInicial - valorDescuento;
-            return totalaPagar;
-
-        }
-    
+   function pagoParqueo(){
+   valorInicial = (horasParqueo*precioB);
+   valorDescuento = valorInicial*porcentDescB;
+   totalaPagar = valorInicial - valorDescuento;
+   return totalaPagar;
+}
