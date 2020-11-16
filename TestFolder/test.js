d8ab674244df61cb8a9126aa0b0dae99ed7f0061
Sebastián <serviciossociologicos@gmail.com>
2020-11-16 17:12:13 -0500

    Se cambio un id y se puso a prueba una funcion js

--- TestFolder/test.js
+++ TestFolder/test.js
@@ -3,13 +3,37 @@
 //Inicio
 //Declarar variables
 
-let horasParqueo
-let valorParqueo
+let horasParqueo;
+let valorParqueo;
+let valorInicial;
+let porcentDesc = 0.10;
+let valorDescuento;
+let totalaPagar;
+let precioA = 2000;
 
 function Calcular()
 {
     //Leer Variables
     //Ingresar horas parqueo
-    horasParqueo = Number(document.getElementById('salida').value);
+    horasParqueo = Number(document.getElementById('time').value);
+
+    //Invocar funcion
+    totalaPagar = pagoParqueo();
+    //valorDescuento = calculoDesc();
+    //valorInicial = precioPark();
+
+    //Imprimir
+    document.getElementById('Total').value= valorInicial - valorDescuento;    
+
 }
-//
+   //Procedimiento
+
+   function pagoParqueo()
+        {
+            valorInicial = horasParqueo*precioA;
+            valorDescuento = valorInicial*porcentDesc;
+            totalaPagar = valorInicial - valorDescuento;
+            return totalaPagar;
+
+        }
+    
