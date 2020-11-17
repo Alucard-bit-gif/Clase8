c9e1fc33a9838f353e0b1cbc920fa44c4b5d1088
Sebastián <serviciossociologicos@gmail.com>
2020-11-16 21:43:51 -0500

    No se ha podido poner en funcion el codigo

--- TestFolder/test.js
+++ TestFolder/test.js
@@ -5,9 +5,8 @@
 
 let tipoVehiculo;
 let horasParqueo;
-let valorParqueo;
 let valorInicial;
-let porcentDesc = 0.10;
+let porcentDescA = 0.10;
 let porcentDescB = 0.15;
 let valorDescuento;
 let totalaPagar;
@@ -15,47 +14,48 @@ let precioA = 2000;
 let precioB = 5000;
 
 
+
 function Calcular()
 {
     //Leer Variables
-    
-
-    
+     
     //Ingresar horas parqueo
     horasParqueo = Number(document.getElementById('time').value);
+    
     //Ingresar tipo Vehiculo
     tipoVehiculo = document.getElementById('carro_moto').value;
-    
 
     //Invocar funcion
+    
     totalaPagar = pagoParqueo();
-    //valorDescuento = calculoDesc();
-    //valorInicial = precioPark();
 
     //Imprimir
     document.getElementById('Inicial').value= valorInicial;    
     document.getElementById('Descuento').value= valorDescuento;    
-    document.getElementById('Total').value= totalaPagar;    
-    console.log(totalaPagar);
+    document.getElementById('Total').value= totalaPagar; 
+    
+    
 }
-   //Procedimiento
+   
 
-   function pagoParqueo(){
+//Procedimiento
 
-    if (tipoVehiculo === 'Carro')
-    {
-   valorInicial = (horasParqueo*precioB);
-   valorDescuento = valorInicial*porcentDescB;
-   totalaPagar = valorInicial - valorDescuento;
-   return totalaPagar;
-    }
-    else 
-    {
-        valorInicial = (horasParqueo*precioA);
-        valorDescuento = valorInicial*porcentDescA;
+
+   
+
+   function pagoParqueo()
+   
+        { 
+        valorInicial = (horasParqueo*precioB);
+        valorDescuento = valorInicial*porcentDescB;
         totalaPagar = valorInicial - valorDescuento;
+        alert('El valor a pagar es:' + totalaPagar)
         return totalaPagar;
-         
-    }
+        }
+        
+        
+   
 
-   }
+    
+
+   
